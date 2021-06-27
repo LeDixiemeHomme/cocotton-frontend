aafa69afb0c1b9740c341fe5850628f097f4b8a7
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-06-27 21:36:41 +0200

    init swift project

---
+++ cocotton-frontend/cocotton-frontend/cocotton_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocotton_frontendApp.swift
+//  cocotton-frontend
+//
+//  Created by Developer on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct cocotton_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
