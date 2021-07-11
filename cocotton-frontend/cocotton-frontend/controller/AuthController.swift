628b31944d245d2de4e2c79602ca16c461a8f211
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-11 19:04:08 +0200

    feat(controller): add application controller manager and auth

--- cocotton-frontend/cocotton-frontend/controller/AuthController.swift
+++ cocotton-frontend/cocotton-frontend/controller/AuthController.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+
+let AUTH_TARGET: String = "auth/"
+
+class AuthController {
+    
+    let rest = RestManager()
+    let authTarget = AUTH_TARGET
+    
+    func register(profile: Profile) -> String? {
+        var profile_id: String = ""
+        
+        rest.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+        rest.httpBodyParameters.add(value: profile.firstName!, forKey: "firstName")
+        rest.httpBodyParameters.add(value: profile.lastName!, forKey: "lastName")
+        rest.httpBodyParameters.add(value: profile.email!, forKey: "email")
+        rest.httpBodyParameters.add(value: profile.username!, forKey: "username")
+        rest.httpBodyParameters.add(value: profile.password!, forKey: "password")
+        rest.httpBodyParameters.add(value: profile.birthDate!, forKey: "birthDate")
+        
+        guard let url = URL(string: API_BASE_URL + AUTH_TARGET + "register") else { return nil }
+        
+        rest.makeRequest(toURL: url, withHttpMethod: .post, token: "no_token") { (results) in
+            guard let response = results.response else { return }
+            if response.httpStatusCode == 201 {
+                guard let location: String = response.headers.value(forKey: "Location") else { return }
+                profile_id = location
+            }
+        }
+        
+        return profile_id
+    }
+    
+    func login(username: String, password: String) -> String? {
+        
+        var responseToken: String? = nil
+        
+        rest.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+        rest.httpBodyParameters.add(value: username, forKey: "username")
+        rest.httpBodyParameters.add(value: password, forKey: "password")
+        
+        guard let url = URL(string: API_BASE_URL + AUTH_TARGET + "login") else { return nil }
+        
+        rest.makeRequest(toURL: url, withHttpMethod: .post, token: "no_token") { (results) in
+            guard let response = results.response else { return }
+            if response.httpStatusCode == 201 {
+                guard let authorization: String = response.headers.value(forKey: "Authorization") else { return }
+                responseToken = authorization
+            }
+        }
+        
+        return responseToken
+    }
+}
--- cocotton-frontend/cocotton-frontend/controller/FileTempController.swift
+++ cocotton-frontend/cocotton-frontend/controller/FileTempController.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+let FILE_TARGET: String = "file/"
+
+class FileTempController {
+    
+    let rest = RestManager()
+    let fileTarget = FILE_TARGET
+    
+    func getFileById(id: String) -> FileTemp {
+        var fileTemp: FileTemp = FileTemp()
+        guard let url = URL(string: API_BASE_URL + FILE_TARGET + id) else { return fileTemp }
+        rest.makeRequest(toURL: url, withHttpMethod: .get, token: TOKEN) { (results) in
+            print("results")
+            print(results)
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let decodedData = try? decoder.decode(FileTemp.self, from: data) else { return }
+                print("decodedData")
+                print(decodedData)
+                print("decodedData")
+                fileTemp = decodedData
+            }
+            
+            print("\n\nResponse HTTP Headers:\n")
+        }
+        print("fileTemp")
+        print(fileTemp)
+        print("fileTemp")
+        return fileTemp
+    }
+}
