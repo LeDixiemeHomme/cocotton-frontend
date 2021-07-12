1427ddab7cab4e71e92ff5379c30250d14493691
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-13 00:46:35 +0200

    feat(view): implement view logic and security

---
+++ cocotton-frontend/cocotton-frontend/view/LoadingView.swift
@@ -0,0 +1,109 @@
+//
+//  LoadingView.swift
+//  cocotton-frontend
+//
+//  Created by Developer on 11/07/2021.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    
+    let timer = Timer.publish(every: 1.6, on: .main, in: .common).autoconnect()
+    @State var leftOffset: CGFloat = -100
+    @State var rightOffset: CGFloat = 100
+    
+    @State var textAction: String
+    
+    @State public var isCredentialsWrong = false
+    
+    private let authController: AuthController = AuthController()
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack{
+                Spacer()
+                
+                TextField("Loading action", text: $textAction)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 32, weight: .semibold))
+                    .foregroundColor(.white)
+                    .padding(8)
+                    .background(Color.black.opacity(0.5))
+                    .cornerRadius(8)
+                    .padding(20)
+                
+                Spacer()
+                
+                ZStack {
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 20, height: 20)
+                        .offset(x: leftOffset)
+                        .animation(Animation.easeInOut(duration: 1))
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 20, height: 20)
+                        .offset(x: leftOffset)
+                        .animation(Animation.easeInOut(duration: 1).delay(0.2))
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 20, height: 20)
+                        .offset(x: leftOffset)
+                        .animation(Animation.easeInOut(duration: 1).delay(0.4))
+                }
+                .onReceive(timer) { (_) in
+                    swap(&self.leftOffset, &self.rightOffset)
+                }
+                
+                Spacer()
+                
+                NavigationLink(
+                    destination: LoginView().onAppear(perform: {
+                        LoginView().displayWrongCredentials()
+                    }), isActive: $isCredentialsWrong,
+                    label: {
+                })
+                
+            }
+            .background(
+                Image("loading_background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .blur(radius: 5)
+            ).edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    func tryLogin(loginCredential: LoginCredential) {
+        print("tryLogin")
+        print(loginCredential)
+        if let _unwrapped: String = authController.login(username: loginCredential.username, password: loginCredential.password) {
+            print(_unwrapped)
+        } else {
+            print("else")
+            isCredentialsWrong = true
+            print("isCredentialsWrong = " + isCredentialsWrong.description)
+        }
+    }
+    
+    func tryRegister(registerCredential: RegisterCredential) {
+        print("tryRegister")
+        let dateFormatter = DateFormatter()
+        let profile: Profile = Profile(lastName: registerCredential.lastName, firstName: registerCredential.firstName, email: registerCredential.email, username: registerCredential.username, password: registerCredential.password, birthDate: dateFormatter.string(from: registerCredential.birthDate))
+        if let _unwrapped: String = authController.register(profile: profile) {
+            print(_unwrapped)
+        } else {
+            print("else")
+            isCredentialsWrong = true
+            print("isCredentialsWrong = " + isCredentialsWrong.description)
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(textAction: "Loading preview")
+    }
+}
--- cocotton-frontend/cocotton-frontend/view/LoginView.swift
+++ cocotton-frontend/cocotton-frontend/view/LoginView.swift
@@ -11,7 +11,10 @@ struct LoginView: View {
     
     @State private var username = ""
     @State private var password = ""
+    
     @State private var emptyMandatoryFields = false
+    @State private var isGoodForLoad = false
+    @State private var isWrongCredentials = false
     
     private let authController: AuthController = AuthController()
 
@@ -28,53 +31,51 @@ struct LoginView: View {
                     .background(Color.black.opacity(0.5))
                     .cornerRadius(8)
                 Spacer()
-                HStack {
-                    Image(systemName: "person.fill").foregroundColor(.black)
-                    TextField("Username", text: $username)
-                    Text("*").foregroundColor(.red)
-                }
-                .padding(.all, 20)
-                .background(Color.white.opacity(0.8))
-                .cornerRadius(8)
-                .padding(.horizontal, 10)
+                Group {
+                    HStack {
+                        Image(systemName: "person.2.fill").foregroundColor(.black)
+                        TextField("Username", text: $username)
+                        Text("*").foregroundColor(.red)
+                    }
+                    .padding(.all, 20)
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+                    
+                    HStack {
+                        Image(systemName: "lock.fill").foregroundColor(.black)
+                        SecureField("Password", text: $password)
+                        Text("*").foregroundColor(.red)
+                    }
+                    .padding(.all, 20)
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
                 
-                HStack {
-                    Image(systemName: "lock.fill").foregroundColor(.black)
-                    SecureField("Password", text: $password)
-                    Text("*").foregroundColor(.red)
+                    HStack() {
+                        Text("*").foregroundColor(.red)
+                        Text(": Mandatory fields.")
+                    }
+                    .padding(.all, 8)
+                    .font(.system(size: 15))
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(8)
                 }
-                .padding(.all, 20)
-                .background(Color.white.opacity(0.8))
-                .cornerRadius(8)
-                .padding(.horizontal, 10)
-                
-                HStack {
-                    Text("*").foregroundColor(.red)
-                    Text(": Mandatory fields.")
-                }
-                .padding(.all, 8)
-                .font(.system(size: 15))
-                .background(Color.white.opacity(0.8))
-                .cornerRadius(8)
-                .padding(.trailing, 220)
-                
+                                
                 NavigationLink(
-                    destination: MainView(),
+                    destination: LoadingView(textAction: "Loging in").onAppear {
+                        LoadingView(textAction: "Loging in").tryLogin(loginCredential: LoginCredential(username: self.username, password: self.password))
+                    }, isActive: $isGoodForLoad,
                     label: {
-    //                    Button(action: {tryLogin()}, label: {
-                            Text("Login")
-                                .foregroundColor(.white)
-                                .font(.system(size: 24, weight: Font.Weight.medium))
-                                .padding()
-                                .background(Color.black.opacity(0.5))
-                                .cornerRadius(8)
-    //                    })
-    //                    .alert(isPresented: $emptyMandatoryFields) {
-    //                        Alert(
-    //                            title: Text("Empty mandatory fields"),
-    //                            message: Text("Mandatory fields must be filled.")
-    //                        )
-    //                    }
+                })
+                
+                Button(action: {viewBehavior()}, label: {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24, weight: Font.Weight.medium))
+                        .padding()
+                        .background(Color.black.opacity(0.5))
+                        .cornerRadius(8)
                 })
                 
                 Spacer()
@@ -95,32 +96,43 @@ struct LoginView: View {
                 
                 
                 Spacer()
+
             }
+            .alert(isPresented: $isWrongCredentials, content: {
+                Alert(
+                    title: Text("Wrong credentials"),
+                    message: Text("Your credentials are wrong. Try creating an account !")
+                )
+            })
             .background(
                 Image("login_background")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .blur(radius: 2)
             ).edgesIgnoringSafeArea(.all)
-            
+        }
+        .alert(isPresented: $emptyMandatoryFields) {
+            Alert(
+                title: Text("Empty mandatory fields"),
+                message: Text("Mandatory fields must be filled.")
+            )
         }
     }
     
-    func tryLogin() {
+    func viewBehavior() {
+        print("viewBehave")
         if username != "" && password != "" {
-            if let _unwrapped: String = authController.login(username: username, password: password) {
-                print(_unwrapped)
-                
-            } else {
-                
-            }
+            isGoodForLoad = true
+            print("isGoodForLoad = " + isGoodForLoad.description)
         } else {
             emptyMandatoryFields = true
+            print("emptyMandatoryFields = " + emptyMandatoryFields.description)
         }
     }
-        
-    func displayRegisterView() {
-        
+    
+    func displayWrongCredentials() {
+        isWrongCredentials = true
+        print("isWrongCredentials = " + isWrongCredentials.description)
     }
     
 }
@@ -128,5 +140,16 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+    }
+}
+
+struct LoginCredential: Codable {
+    
+    let username: String
+    let password: String
+    
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
     }
 }
