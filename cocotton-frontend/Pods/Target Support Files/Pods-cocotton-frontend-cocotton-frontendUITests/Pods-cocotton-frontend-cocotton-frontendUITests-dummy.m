5b48fabc4e64c69fb433927902552952bdc279e2
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-13 23:42:56 +0200

    feat(cocoapods-keychainwrapper): project files for cocoapods and keychainwrapper

---
+++ cocotton-frontend/Pods/Target Support Files/Pods-cocotton-frontend-cocotton-frontendUITests/Pods-cocotton-frontend-cocotton-frontendUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocotton_frontend_cocotton_frontendUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cocotton_frontend_cocotton_frontendUITests
+@end
---
+++ cocotton-frontend/Pods/Target Support Files/Pods-cocotton-frontend/Pods-cocotton-frontend-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocotton_frontend : NSObject
+@end
+@implementation PodsDummy_Pods_cocotton_frontend
+@end
