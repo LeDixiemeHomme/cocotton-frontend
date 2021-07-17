c9e01870365afd970d18f94ab66040894c8f7987
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-17 19:38:30 +0200

    feat(mocking): add json loader to parse json file

---
+++ cocotton-frontend/cocotton-frontend/controller/JsonLoader.swift
@@ -0,0 +1,30 @@
+//
+//  JsonLoader.swift
+//  cocotton-frontend
+//
+//  Created by Developer on 17/07/2021.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: ".json")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
