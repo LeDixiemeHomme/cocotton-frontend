64f1abc53a4a9d6cd4efd101b3eb1cd1b30df8c4
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-19 02:20:31 +0200

    feat(add-recipe): create view for creating recipe work in progress

--- cocotton-frontend/cocotton-frontend/view/recipe/RecipeCreateView.swift
+++ cocotton-frontend/cocotton-frontend/view/recipe/RecipeCreateView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct RecipeCreateView: View {
+    
+    private let backGround: String = "add_background"
+    @State private var displayAlert = false
+    @State private var alert: Alert = Alert(title: Text("Empty alert"))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Work in progress ...")
+                .font(.system(size: 48, weight: .semibold))
+                .foregroundColor(.white)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .background(Color.black.opacity(0.5))
+                .cornerRadius(8)
+            Spacer()
+        }
+        .background(
+            Image(self.backGround)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .blur(radius: 2)
+        ).edgesIgnoringSafeArea(.all)
+        .alert(isPresented: $displayAlert, content: {
+            self.alert
+        })
+        .onAppear{
+            alert = Alert(
+                title: Text("Coming soon"),
+                message: Text("You will be able to create recipe on this page !")
+            )
+            self.displayAlert = true
+        }
     }
 }
 
