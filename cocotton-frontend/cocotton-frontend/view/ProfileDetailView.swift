0979a89b132b9c1f5234c6054309ad9f46bd2ae1
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-19 02:09:20 +0200

    feat(prod-token): add dependency JWTDecode to retrieve user id from a session token

---
+++ cocotton-frontend/cocotton-frontend/view/ProfileDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileDetailView.swift
+//  cocotton-frontend
+//
+//  Created by Developer on 19/07/2021.
+//
+
+import SwiftUI
+
+struct ProfileDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileDetailView()
+    }
+}
---
+++ cocotton-frontend/cocotton-frontend/view/recipe/RecipeCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCreateView.swift
+//  cocotton-frontend
+//
+//  Created by Developer on 19/07/2021.
+//
+
+import SwiftUI
+
+struct RecipeCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCreateView()
+    }
+}
