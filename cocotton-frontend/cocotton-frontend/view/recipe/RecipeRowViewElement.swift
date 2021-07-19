c6949b159f4dda5bd88118099ea8d5120077aa7a
LeDixiemeHome <benoit.valleea@hotmail.fr>
2021-07-20 00:06:08 +0200

    fix(recipe-list-number): add recipe number to list

--- cocotton-frontend/cocotton-frontend/view/recipe/RecipeRowViewElement.swift
+++ cocotton-frontend/cocotton-frontend/view/recipe/RecipeRowViewElement.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct RecipeRowViewElement: View {
     public var recipe: Recipe
+    public var recipeNumber: Int
 
     var body: some View {
         HStack {
             Image("recipe_row_image")
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(recipe.title)
+            Text(" \(recipeNumber.description). \(recipe.title)")
 
             Spacer()
         }
@@ -24,6 +25,6 @@ struct RecipeRowViewElement: View {
 
 struct RecipeRowView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeRowViewElement(recipe: Recipe(id: "", title: "", people: 1, ingredients: ["String"], steps: ["String"], userId: ""))
+        RecipeRowViewElement(recipe: Recipe(id: "", title: "", people: 1, ingredients: ["String"], steps: ["String"], userId: ""), recipeNumber: 0)
     }
 }
